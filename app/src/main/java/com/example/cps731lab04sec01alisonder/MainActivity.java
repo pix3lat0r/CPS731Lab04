7bad353f8e7a204d28143ed30557ec03cc77aeb1
AlisonD <itsbasictome@gmail.com>
2020-10-03 22:54:34 -0400

    modified code

--- app/src/main/java/com/example/cps731lab04sec01alisonder/MainActivity.java
+++ app/src/main/java/com/example/cps731lab04sec01alisonder/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.cps731lab04sec01alisonder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] recipeTitles = new String[]{"Ice Coffee", "French Vanilla", "Earl Grey Tea"};
+    RecyclerView recyclerView;
+    String titles[], txt[];
+    int images[] = {R.drawable.ic, R.drawable.fv, R.drawable.eg};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        titles = getResources().getStringArray(R.array.recipeTitles);
+        txt = getResources().getStringArray(R.array.descriptions);
+
+        RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(this, titles, txt, images);
     }
 }
--- app/src/main/java/com/example/cps731lab04sec01alisonder/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/cps731lab04sec01alisonder/RecyclerViewAdapter.java
@@ -1,4 +1,44 @@
 package com.example.cps731lab04sec01alisonder;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+    public RecyclerViewAdapter(Context ct, String[] titles, String[] txt, int[] img) {
+        context = ct;
+        data1 = titles;
+        data2 = txt;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
 }
